8b75171ab349d2fb0998be6993d964b5f1b3625b
Maria Kjellholm <maria.kjellholm@gmail.com>
2024-10-21 21:21:12 +0200

    images

---
+++ maria.js
@@ -0,0 +1,268 @@
+const cardDeck = [
+    {
+        name: 'Ace of clubs',
+        value: undefined,
+        image: './img/clubs-A.png'
+    },
+    {
+        name: 'King of clubs',
+        value: 10,
+        image: './img/clubs-K.png'
+    },
+    {
+        name: 'Queen of clubs',
+        value: 10,
+        image: './img/clubs-Q.png'
+    },
+    {
+        name: 'Jack of clubs',
+        value: 10,
+        image: './img/clubs-J.png'
+    },
+    {
+        name: 'Ten of clubs',
+        value: 10,
+        image: './img/clubs-10.png'
+    },
+    {
+        name: 'Nine of clubs',
+        value: 9,
+        image: './img/clubs-9.png'
+    },
+    {
+        name: 'Eight of clubs',
+        value: 8,
+        image: './img/clubs-8.png'
+    },
+    {
+        name: 'Seven of clubs',
+        value: 7,
+        image: './img/clubs-7.png'
+    },
+    {
+        name: 'Six of clubs',
+        value: 6,
+        image: './img/clubs-6.png'
+    },
+    {
+        name: 'Five of clubs',
+        value: 5,
+        image: './img/clubs-5.png'
+    },
+    {
+        name: 'Four of clubs',
+        value: 4,
+        image: './img/clubs-4.png'
+    },
+    {
+        name: 'Three of clubs',
+        value: 3,
+        image: './img/clubs-3.png'
+    },
+    {
+        name: 'Two of clubs',
+        value: 2,
+        image: './img/clubs-2.png'
+    },
+
+    {
+        name: 'Ace of diamonds',
+        value: undefined,
+        image: './img/diamonds-A.png'
+    },
+    {
+        name: 'King of diamonds',
+        value: 10,
+        image: './img/diamonds-K.png'
+    },
+    {
+        name: 'Queen of diamonds',
+        value: 10,
+        image: './img/diamonds-Q.png'
+    },
+    {
+        name: 'Jack of diamonds',
+        value: 10,
+        image: './img/club-J.png'
+    },
+    {
+        name: 'Ten of diamonds',
+        value: 10,
+        image: './img/club-10.png'
+    },
+    {
+        name: 'Nine of diamonds',
+        value: 9,
+        image: './img/club-9.png'
+    },
+    {
+        name: 'Eight of diamonds',
+        value: 8,
+        image: './img/club-8.png'
+    },
+    {
+        name: 'Seven of diamonds',
+        value: 7,
+        image: './img/club-7.png'
+    },
+    {
+        name: 'Six of diamonds',
+        value: 6,
+        image: './img/club-6.png'
+    },
+    {
+        name: 'Five of diamonds',
+        value: 5,
+        image: './img/club-5.png'
+    },
+    {
+        name: 'Four of diamonds',
+        value: 4,
+        image: './img/club-4.png'
+    },
+    {
+        name: 'Three of diamonds',
+        value: 3,
+        image: './img/club-3.png'
+    },
+    {
+        name: 'Two of diamonds',
+        value: 2,
+        image: './img/club-2.png'
+    },
+
+    {
+        name: 'Ace of spades',
+        value: undefined,
+        image: './img/club-A.png'
+    },
+    {
+        name: 'King of spades',
+        value: 10,
+        image: './img/club-K.png'
+    },
+    {
+        name: 'Queen of spades',
+        value: 10,
+        image: './img/club-Q.png'
+    },
+    {
+        name: 'Jack of spades',
+        value: 10,
+        image: './img/club-J.png'
+    },
+    {
+        name: 'Ten of spades',
+        value: 10,
+        image: './img/club-10.png'
+    },
+    {
+        name: 'Nine of spades',
+        value: 9,
+        image: './img/club-9.png'
+    },
+    {
+        name: 'Eight of spades',
+        value: 8,
+        image: './img/club-8.png'
+    },
+    {
+        name: 'Seven of spades',
+        value: 7,
+        image: './img/club-7.png'
+    },
+    {
+        name: 'Six of spades',
+        value: 6,
+        image: './img/club-6.png'
+    },
+    {
+        name: 'Five of spades',
+        value: 5,
+        image: './img/club-5.png'
+    },
+    {
+        name: 'Four of spades',
+        value: 4,
+        image: './img/club-4.png'
+    },
+    {
+        name: 'Three of spades',
+        value: 3,
+        image: './img/club-3.png'
+    },
+    {
+        name: 'Two of spades',
+        value: 2,
+        image: './img/club-2.png'
+    },
+
+    {
+        name: 'Ace of hearts',
+        value: undefined,
+        image: './img/club-A.png'
+    },
+    {
+        name: 'King of hearts',
+        value: 10,
+        image: './img/club-K.png'
+    },
+    {
+        name: 'Queen of hearts',
+        value: 10,
+        image: './img/club-Q.png'
+    },
+    {
+        name: 'Jack of hearts',
+        value: 10,
+        image: './img/club-J.png'
+    },
+    {
+        name: 'Ten of hearts',
+        value: 10,
+        image: './img/club-10.png'
+    },
+    {
+        name: 'Nine of hearts',
+        value: 9,
+        image: './img/club-9.png'
+    },
+    {
+        name: 'Eight of hearts',
+        value: 8,
+        image: './img/club-8.png'
+    },
+    {
+        name: 'Seven of hearts',
+        value: 7,
+        image: './img/club-7.png'
+    },
+    {
+        name: 'Six of hearts',
+        value: 6,
+        image: './img/club-6.png'
+    },
+    {
+        name: 'Five of hearts',
+        value: 5,
+        image: './img/club-5.png'
+    },
+    {
+        name: 'Four of hearts',
+        value: 4,
+        image: './img/club-4.png'
+    },
+    {
+        name: 'Three of hearts',
+        value: 3,
+        image: './img/club-3.png'
+    },
+    {
+        name: 'Two of hearts',
+        value: 2,
+        image: './img/club-2.png'
+    },
+
+]
+
+console.log(cardname[0])
